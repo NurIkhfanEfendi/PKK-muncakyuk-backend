bd85200f692c0f2716f92e0281b18b1a00ad0954
DimasAfifRehardendi <dimas_afif_27rpl@student.smktelkom-mlg.sch.id>
2020-04-08 22:22:23 +0700

    Fix Login

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,17 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->text('password');
+            $table->text('password_verify');
             $table->rememberToken();
             $table->timestamps();
         });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@store');
+
+Route::group(['middleware' => ['jwt.verify']], function () {
+    Route::post('logout', 'UserController@logout');
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
